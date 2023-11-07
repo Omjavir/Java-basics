2dc5af6839d77e05fd222372683564cd18e4ab16
Om Suresh Javir <javirom07042003@gmail.com>
2023-11-07 22:07:51 +0530

    java cheatsheet-tutorial

---
+++ Hello_World.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Hello_World {
+    public static void main(String[] args) {
+        // *** Printing Hello World
+        System.out.println("Hello World");
+
+        // *** Taking the input
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int sum = a + b;
+        // System.out.println("AGE :: " + sum);
+
+        // ***Modulo
+        // int x=10%3; // 3 x 3 = 9; 10 - 9 = 1; :: => 1
+        // System.out.println("MODULO :: " + x);
+
+        // *** Loops
+        // *FOR LOOP*
+        // for(int i = 0; i <= 10; i++){
+        // System.out.println("i :: " + i);
+        // }
+
+        // *WHILE LOOP*
+        // int i = 1;
+        // while(i <= 10){
+        // System.out.println("i :: " + i);
+        // i++;
+        // }
+
+        // *BREAK STATEMENT*
+        // for(int i = 0; i < 10; i++){
+        // System.out.println("i :: " + i);
+        // if(i == 8){
+        // break;
+        // }
+        // }
+
+        // *TRY CATCH*
+        // try {
+        //     int divideByZero = 5 / 0;
+        //     System.out.println("Rest of code in try block");
+        // }
+        // catch (ArithmeticException e) {
+        //     System.out.println("ArithmeticException => " + e.getMessage());
+        // } finally {
+        //     System.out.println("Finally statement run");
+        // }
+
+    }
+}
